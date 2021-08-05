69978cd65f4bdc4471c8b9114cf81e3c8815ad86
KiYoung <kykim818@naver.com>
2021-08-05 22:49:15 +0900

    chore

--- redux-typescript/src/App.tsx
+++ redux-typescript/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from './components/atoms/Button';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <h1>Button</h1>
+      <Button>Button Components</Button>
     </div>
   );
 }
